c037fb1e057d49c39c9424ab3c0a688cc7064ddb
LyubomirT <terlyubomir@gmail.com>
2025-09-13 17:21:55 +0300

    RELEASE: Update version to 1.5.0

--- src/gui.py
+++ src/gui.py
@@ -15,7 +15,7 @@ from core import get_state_manager, StateEvent
 from config.config_manager import ConfigManager
 from config.config_ui_generator import ConfigUIGenerator
 
-__version__ = "1.4.4" 
+__version__ = "1.5.0" 
 
 # Local GUI state (not shared across modules)
 root = None
